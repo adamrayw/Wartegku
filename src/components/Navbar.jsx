305665afe2f1bf4300fef3b4391ace21fea6f0fc
Adam Ray <raywibowo68@gmail.com>
2022-02-10 15:50:48 +0700

    change sm to md, remove px-2, change px-4 to px-2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <nav className="bg-white shadow-sm border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <nav className="bg-white shadow-sm border-gray-200  md:px-0 px-2 py-2.5 rounded dark:bg-gray-800">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="/" className="flex p-2">
                     <span className="self-center text-2xl font-bold whitespace-nowrap text-blue-500">Wartegku</span>
@@ -15,14 +15,14 @@ export default function Navbar() {
                         <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </button>
                 </div>
-                <div className="hidden justify-between items-center w-full p-4 md:flex md:w-auto md:order-1" id="mobile-menu-3">
+                <div className="hidden justify-between items-center w-full md:p-0 p-4 md:flex md:w-auto md:order-1" id="mobile-menu-3">
                     <div className="relative md:mt-0 mt-4 md:block">
                         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                             <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                         </div>
                         <input type="text" id="email-adress-icon" className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
                     </div>
-                    <div className='flex md:justify-center justify-end items-center space-x-6 md:pl-6 md:mt-0 mt-6'>
+                    <div className='flex md:justify-center justify-end items-center space-x-8 md:pl-8 md:mt-0 mt-6'>
                         <div className='relative'>
                             <div className='absolute z-10 h-4 w-4 rounded-full bg-blue-500 text-white text-xs right-0 top-0 translate-x-2/4 -translate-y-1/2 mb-10'>1</div>
                             <i className="fa-solid fa-cart-shopping fa-lg"></i>
