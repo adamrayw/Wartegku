2ab21747ed6c45a63bca1393bd92931660d5e023
Adam Ray <raywibowo68@gmail.com>
2022-02-10 19:47:40 +0700

    Optimize responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <nav className="bg-white shadow-sm border-gray-200  md:px-0 px-2 py-2.5 rounded dark:bg-gray-800">
+        <nav className="bg-white shadow-sm border-gray-200 md:px-6 px-4 py-4 rounded dark:bg-gray-800">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
-                <a href="/" className="flex p-2">
-                    <span className="self-center text-2xl font-bold whitespace-nowrap text-blue-500">Wartegku</span>
-                </a>
+                <Link to="/" className="flex ">
+                    <span className="self-center md:text-2xl text-xl font-bold whitespace-nowrap text-blue-500">Wartegku</span>
+                </Link>
                 <div className="flex md:order-2 md:hidden">
                     <button data-collapse-toggle="mobile-menu-3" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-3" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
@@ -15,7 +16,7 @@ export default function Navbar() {
                         <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </button>
                 </div>
-                <div className="hidden justify-between items-center w-full md:p-0 p-4 md:flex md:w-auto md:order-1" id="mobile-menu-3">
+                <div className="hidden justify-between items-center w-full md:p-0 py-4 md:flex md:w-auto md:order-1" id="mobile-menu-3">
                     <div className="relative md:mt-0 mt-4 md:block">
                         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                             <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
