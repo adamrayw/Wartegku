4299f8321a5a070540a4659fcc420fb6d9a3885d
Adam Ray <raywibowo68@gmail.com>
2022-02-10 15:36:36 +0700

    add home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App max-w-4xl mx-auto">
       <Navbar />
+      <Home />
     </div>
   );
 }
