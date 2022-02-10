66a635f0b7e34d9e508a31117621ae361ff66e48
Adam Ray <raywibowo68@gmail.com>
2022-02-10 19:46:26 +0700

    Integration Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App max-w-4xl mx-auto">
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="detail" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
