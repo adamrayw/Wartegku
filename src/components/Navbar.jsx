739d4901902276353a678a3d8207e3881e86f60a
Adam Ray <raywibowo68@gmail.com>
2022-02-14 23:13:21 +0700

    Display cart in mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,11 @@ export default function Navbar() {
                 <Link to="/" className="flex ">
                     <span className="self-center md:text-2xl text-xl font-bold whitespace-nowrap text-blue-500">Wartegku</span>
                 </Link>
-                <div className="flex md:order-2 md:hidden">
+                <div className="flex items-center md:order-2 md:hidden">
+                    <Link to="/cart" className='relative'>
+                        <div className='absolute z-10 h-4 w-4 rounded-full bg-blue-500 text-white text-xs right-0 top-0 translate-x-2/4 -translate-y-1/2 mb-10'>{cartTotalQuantity}</div>
+                        <i className="fa-solid fa-cart-shopping fa-lg"></i>
+                    </Link>
                     <button data-collapse-toggle="mobile-menu-3" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-3" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
