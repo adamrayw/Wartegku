5be483e488f07a48f2b0aa354bb9f1d73f4c2a8f
Adam Ray <raywibowo68@gmail.com>
2022-02-15 14:59:40 +0700

    add margin top bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="mt-10 bg-white rounded-lg flex md:flex-row flex-col-reverse items-center md:justify-between md:p-0 p-6 dark:bg-gray-800">
+        <footer className="my-10 bg-white rounded-lg flex md:flex-row flex-col-reverse items-center md:justify-between md:p-0 p-6 dark:bg-gray-800">
             <span className="text-xs text-gray-500 sm:text-center mt-4 md:mt-0 dark:text-gray-400">© 2022 <a href="https://adamrw.me" className="hover:underline" rel="noreferrer" target="_blank">Wartegku™</a>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 sm:mt-0">
