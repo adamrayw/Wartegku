460013877f811c731e511f9d36c5a79dd2e9e583
Adam Ray <raywibowo68@gmail.com>
2022-02-19 23:49:41 +0700

    Change baseUrl

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://warteg.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 axios.defaults.withCredentials = true;
