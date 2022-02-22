b0199960917fa511a28b023d4f44f565bdcfd821
Adam Ray <raywibowo68@gmail.com>
2022-02-22 09:00:35 +0700

    Add new order features

---
+++ src/redux/features/myOrder/order.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: [],
+};
+
+export const orderSlice = createSlice({
+  name: "order",
+  initialState,
+  reducers: {},
+});
