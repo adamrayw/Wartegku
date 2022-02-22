2c0900b10843cb54c23fc506f46d98180227fd01
Adam Ray <raywibowo68@gmail.com>
2022-02-22 17:14:23 +0700

    add order reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cart from "./features/amountCart/cart";
+import order from "./features/myOrder/order";
 
 export const store = configureStore({
   reducer: {
     cart: cart,
+    order: order,
   },
 });
