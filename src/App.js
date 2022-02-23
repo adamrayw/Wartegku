86a3f4204141375143c2cc7dcc457d5c29cc77df
Adam Ray <raywibowo68@gmail.com>
2022-02-24 01:07:19 +0700

    Remove detail2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import Footer from "./components/Footer";
 import Detail from "./pages/Detail";
-import Detail2 from "./pages/Detail2";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
@@ -23,7 +22,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="detail" element={<Detail />} />
-        <Route path="detail2" element={<Detail2 />} />
         <Route path="cart" element={<Cart />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
