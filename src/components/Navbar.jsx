1c71ff2f50348edef3b3049baff948e8924f6dc9
Adam Ray <raywibowo68@gmail.com>
2022-02-25 00:44:22 +0700

    using Search component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import Search from './Search';
 
 export default function Navbar() {
 
@@ -46,12 +47,7 @@ export default function Navbar() {
                     </button>
                 </div>
                 <div className="hidden justify-between items-center w-full md:p-0 py-4 md:flex md:w-auto md:order-1" id="mobile-menu-3">
-                    <div className="relative md:mt-0 mt-4 md:block">
-                        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                            <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
-                        </div>
-                        <input type="text" id="email-adress-icon" className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
-                    </div>
+                    <Search />
                     <div className='flex md:justify-center justify-end items-center space-x-8 md:pl-8 md:mt-0 mt-6'>
                         <Link to="/cart" className='relative md:block hidden'>
                             <div className='absolute z-10 h-5 w-5 flex justify-center items-center rounded-full border bg-blue-700 text-white text-xs right-0 top-0 translate-x-2/4 -translate-y-1/2 mb-10'>{cartTotalQuantity}</div>
