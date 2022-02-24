093176d74f5e432e8db6a9848e71d946f8323d3a
Adam Ray <raywibowo68@gmail.com>
2022-02-25 00:42:54 +0700

    Data Menu

---
+++ src/data/menu.js
@@ -0,0 +1,47 @@
+import gadogado from "../assets/menu/gadogado.png";
+import kentanggoreng from "../assets/menu/kentanggoreng.png";
+import tahuikan from "../assets/menu/tahuikan.png";
+import kopiespresso from "../assets/menu/kopiespresso.png";
+
+export const menu = [
+  {
+    id: 1,
+    name: "Gado Gado",
+    image: gadogado,
+    price: "10000",
+    ori_price: "15000",
+    description:
+      "Gado - gado adalah makanan khas Jakarta berisi sayur-sayuran yang direbus, irisan telur dan tahu, serta ditaburi bawang goreng dan kerupuk. Sayur-sayuran ditambahkan dengan bumbu kacang atau saus dari kacang tanah yang telah dihaluskan yang kemudian diaduk merata.",
+    qty: 0,
+  },
+  {
+    id: 2,
+    name: "Kentang Goreng",
+    image: kentanggoreng,
+    price: "20000",
+    ori_price: "25000",
+    description:
+      "Kentang goreng adalah hidangan yang dibuat dari potongan-potongan kentang yang digoreng dalam minyak goreng panas. Di dalam menu rumah-rumah makan, kentang goreng yang dipotong panjang-panjang dan digoreng dalam keadaan terendam di dalam minyak goreng panas disebut French fries.",
+    qty: 0,
+  },
+  {
+    id: 3,
+    name: "Tahu Ikan",
+    image: tahuikan,
+    price: "8000",
+    ori_price: "10000",
+    description:
+      "Tahu ikan adalah makanan khas Indonesia yang terbuat dari tahu dan ikan. Tahu ikan biasanya dibuat dengan menggunakan tahu yang sudah dihaluskan dan ditambahkan dengan ikan yang sudah dihaluskan. Tahu ikan biasanya dibuat dengan menggunakan tahu yang sudah dihaluskan dan ditambahkan dengan ikan yang sudah dihaluskan.",
+    qty: 0,
+  },
+  {
+    id: 4,
+    name: "Kopi Espresso",
+    image: kopiespresso,
+    price: "45000",
+    ori_price: "50000",
+    description:
+      "Kopi espresso adalah kopi yang dibuat dengan menggunakan espresso machine. Kopi espresso biasanya dibuat dengan menggunakan kopi yang sudah dihaluskan dan ditambahkan dengan santan kental.",
+    qty: 0,
+  },
+];
