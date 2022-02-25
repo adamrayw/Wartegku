39412a962c4a31b69046f4a3f67a670ee181be81
Adam Ray <raywibowo68@gmail.com>
2022-02-25 16:23:11 +0700

    add message if empty

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -51,7 +51,16 @@ export default function Search() {
                                 </Link>
                             </li>
                         ))}
-
+                        {search.length === 0 && (
+                            <li>
+                                <div className='p-4 text-sm text-gray-400 text-center'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mb-1 mx-auto text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                    Coba dengan keyword lain
+                                </div>
+                            </li>
+                        )}
                     </ul>
                 </div>
             )}
